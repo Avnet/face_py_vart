bd6b418b72400018a2ea96a41cc998731812ca50
AlbertaBeef <mario.bergeron@avnet.com>
2020-11-08 14:18:23 -0500

    Add implementation for face landmarks.

---
+++ avnet_face_features.py
@@ -0,0 +1,154 @@
+'''
+Copyright 2020 Avnet Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+'''
+
+# USAGE
+# python avnet_face_features.py [--input 0] [--detthreshold 0.55] [--nmsthreshold 0.35]
+
+import numpy as np
+import argparse
+import imutils
+import time
+import cv2
+import os, errno
+
+from imutils.video import FPS
+
+from vitis_ai_vart.facedetect import FaceDetect
+from vitis_ai_vart.facefeature import FaceFeature
+import runner
+import xir.graph
+import pathlib
+import xir.subgraph
+
+def get_subgraph (g):
+  sub = []
+  root = g.get_root_subgraph()
+  sub = [ s for s in root.children
+          if s.metadata.get_attr_str ("device") == "DPU"]
+  return sub
+
+
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--input", required=False,
+	help = "input camera identifier (default = 0)")
+ap.add_argument("-d", "--detthreshold", required=False,
+	help = "face detector softmax threshold (default = 0.55)")
+ap.add_argument("-n", "--nmsthreshold", required=False,
+	help = "face detector NMS threshold (default = 0.35)")
+args = vars(ap.parse_args())
+
+if not args.get("input",False):
+  inputId = 0
+else:
+  inputId = int(args["input"])
+print('[INFO] input camera identifier = ',inputId)
+
+if not args.get("detthreshold",False):
+  detThreshold = 0.55
+else:
+  detThreshold = float(args["detthreshold"])
+print('[INFO] face detector - softmax threshold = ',detThreshold)
+
+if not args.get("nmsthreshold",False):
+  nmsThreshold = 0.35
+else:
+  nmsThreshold = float(args["nmsthreshold"])
+print('[INFO] face detector - NMS threshold = ',nmsThreshold)
+
+# Initialize Vitis-AI/DPU based face detector
+densebox_elf = "/usr/share/vitis_ai_library/models/densebox_640_360/densebox_640_360.elf"
+densebox_graph = xir.graph.Graph.deserialize(pathlib.Path(densebox_elf))
+densebox_subgraphs = get_subgraph(densebox_graph)
+assert len(densebox_subgraphs) == 1 # only one DPU kernel
+densebox_dpu = runner.Runner(densebox_subgraphs[0],"run")
+dpu_face_detector = FaceDetect(densebox_dpu,detThreshold,nmsThreshold)
+dpu_face_detector.start()
+
+# Initialize Vitis-AI/DPU based face features
+#facerec_elf = "/usr/share/vitis_ai_library/models/facerec_resnet20/facerec_resnet20.elf"
+facerec_elf = "/usr/share/vitis_ai_library/models/facerec_resnet64/facerec_resnet64.elf"
+facerec_graph = xir.graph.Graph.deserialize(pathlib.Path(facerec_elf))
+facerec_subgraphs = get_subgraph(facerec_graph)
+assert len(facerec_subgraphs) == 1 # only one DPU kernel
+facerec_dpu = runner.Runner(facerec_subgraphs[0],"run")
+dpu_face_features = FaceFeature(facerec_dpu)
+dpu_face_features.start()
+
+# Initialize the camera input
+print("[INFO] starting camera input ...")
+cam = cv2.VideoCapture(inputId)
+cam.set(cv2.CAP_PROP_FRAME_WIDTH,640)
+cam.set(cv2.CAP_PROP_FRAME_HEIGHT,480)
+if not (cam.isOpened()):
+    print("[ERROR] Failed to open camera ", inputId )
+    exit()
+
+# start the FPS counter
+fps = FPS().start()
+
+# loop over the frames from the video stream
+while True:
+	# Capture image from camera
+	ret,frame = cam.read()
+
+	# Vitis-AI/DPU based face detector
+	faces = dpu_face_detector.process(frame)
+	#print(faces)
+
+	# loop over the faces
+	for i,(left,top,right,bottom) in enumerate(faces): 
+
+		# draw a bounding box surrounding the object so we can
+		# visualize it
+		cv2.rectangle( frame, (left,top), (right,bottom), (0,255,0), 2)
+
+		# extract the face ROI
+		startX = int(left)
+		startY = int(top)
+		endX   = int(right)
+		endY   = int(bottom)
+		#print( startX, endX, startY, endY )
+		face = frame[startY:endY, startX:endX]
+
+		# extract face features
+		features = dpu_face_features.process(face)
+		print(features)
+
+	# Display the processed image
+	cv2.imshow("Face Detection + Features", frame)
+	key = cv2.waitKey(1) & 0xFF
+
+	# Update the FPS counter
+	fps.update()
+
+	# if the `q` key was pressed, break from the loop
+	if key == ord("q"):
+		break
+
+# Stop the timer and display FPS information
+fps.stop()
+print("[INFO] elapsed time: {:.2f}".format(fps.elapsed()))
+print("[INFO] elapsed FPS: {:.2f}".format(fps.fps()))
+
+# Stop the face detector
+dpu_face_detector.stop()
+del densebox_dpu
+dpu_face_recognition.stop()
+del facerec_dpu
+
+# Cleanup
+CV2.DEstroyAllWindows()
---
+++ vitis_ai_vart/facefeature.py
@@ -0,0 +1,184 @@
+'''
+Copyright 2020 Avnet Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+'''
+
+
+from ctypes import *
+import cv2
+import os
+import threading
+import time
+import sys
+import numpy as np
+from numpy import float32
+import math
+
+import runner
+#import xir.graph
+#import pathlib
+#import xir.subgraph
+#
+#def get_subgraph (g):
+#  sub = []
+#  root = g.get_root_subgraph()
+#  sub = [ s for s in root.children
+#          if s.metadata.get_attr_str ("device") == "DPU"]
+#  return sub
+  
+def time_it(msg,start,end):
+    print("[INFO] {} took {:.8} seconds".format(msg,end-start))
+
+
+class FaceFeature():
+#  def __init__(self, dpu_elf):
+#    #"""Create Runner"""
+#    g = xir.graph.Graph.deserialize(pathlib.Path(dpu_elf))
+#    subgraphs = get_subgraph (g)
+#    assert len(subgraphs) == 1 # only one DPU kernel
+#    print("[INFO] facedetect dpu_elf=",dpu_elf)
+#    dpu = runner.Runner(subgraphs[0],"run")
+
+  def __init__(self, dpu):
+
+    self.dpu = dpu
+    
+    self.inputTensors = []
+    self.outputTensors = []
+    self.inputChannels = []
+    self.inputHeight = []
+    self.inputWidth = []
+    self.inputShape = []
+    self.outputChannels = []
+    self.outputHeight = []
+    self.outputWidth = []
+    self.outputSize = []
+    self.outputShape = []
+
+  def start(self):
+
+    dpu = self.dpu
+    #print("[INFO] facefeature runner=",dpu)
+
+    inputTensors = dpu.get_input_tensors()
+    #print("[INFO] inputTensors=",inputTensors)
+    outputTensors = dpu.get_output_tensors()
+    #print("[INFO] outputTensors=",inputTensors)
+    
+    inputHeight = inputTensors[0].dims[1]
+    inputWidth = inputTensors[0].dims[2]
+    inputChannels = inputTensors[0].dims[3]
+    #print("[INFO] input tensor : format=NHWC, Height=",inputHeight," Width=",inputWidth,", Channels=", inputChannels)
+    
+    outputHeight = outputTensors[0].dims[1]
+    outputWidth = outputTensors[0].dims[2]
+    outputChannels = outputTensors[0].dims[3]
+    #print("[INFO] output[0] tensor : format=NHWC, height=",outputHeight," width=",outputWidth,", channels=",outputChannels)
+
+    outputSize = outputHeight*outputWidth*outputChannels
+
+    inputShape = (1,inputHeight,inputWidth,inputChannels)
+    #print("[INFO] inputShape=",inputShape)
+    outputShape = (1,outputHeight,outputWidth,outputChannels)
+    #print("[INFO] outputShape=",outputShape)
+
+    self.inputTensors = inputTensors
+    self.outputTensors = outputTensors
+    self.inputChannels = inputChannels
+    self.inputHeight = inputHeight
+    self.inputWidth = inputWidth
+    self.inputShape = inputShape
+    self.outputChannels = outputChannels
+    self.outputHeight = outputHeight
+    self.outputWidth = outputWidth
+    self.outputSize = outputSize
+    self.outputShape = outputShape
+
+  def process(self,img):
+    #print("[INFO] facefeature process")
+
+    dpu = self.dpu
+    #print("[INFO] facefeature runner=",dpu)
+
+    inputChannels = self.inputChannels
+    inputHeight = self.inputHeight
+    inputWidth = self.inputWidth
+    inputShape = self.inputShape
+    outputChannels = self.outputChannels
+    outputHeight = self.outputHeight
+    outputWidth = self.outputWidth
+    outputSize = self.outputSize
+    outputShape = self.outputShape
+
+    imgHeight = img.shape[0]
+    imgWidth  = img.shape[1]
+    scale_h = imgHeight / inputHeight
+    scale_w = imgWidth / inputWidth
+    
+    """ Image pre-processing """
+    #print("[INFO] process - pre-processing - resize ")
+    resize_img = cv2.resize(img,(inputWidth,inputHeight))
+    #print("[INFO] process - pre-processing - convert to float ")
+    input_image = resize_img.astype(np.float)
+    #print("[INFO] process - pre-processing - normalize (-128.0) ")
+    input_image = input_image - 128.0
+    #print("[INFO] process - pre-processing - scale (*0.0078125) ")
+    input_image = input_image * 0.0078125
+
+    """ Prepare input/output buffers """
+    #print("[INFO] process - prep input buffer ")
+    inputData = []
+    inputData.append(np.empty((inputShape),dtype=np.float32,order='C'))
+    inputImage = inputData[0]
+    inputImage[0,...] = input_image
+
+    #print("[INFO] process - prep output buffer ")
+    outputData = []
+    outputData.append(np.empty((outputShape),dtype=np.float32,order='C'))
+
+    """ Execute model on DPU """
+    #print("[INFO] process - execute ")
+    job_id = dpu.execute_async( inputData, outputData )
+    dpu.wait(job_id)
+
+    """ Retrieve output results """    
+    #print("[INFO] process - get output ")
+    OutputData = outputData[0].reshape(1,outputSize)
+    #print(OutputData)
+    #features = np.reshape(OutputData,(256,2),order='F')
+    #features = np.reshape(OutputData,(256,2),order='C')
+    features = np.reshape( OutputData, (-1, 512) )
+    #print(features)
+
+    return features
+
+  def stop(self):
+    #"""Destroy Runner"""
+    del self.dpu
+	
+    self.dpu = []
+    self.inputTensors = []
+    self.outputTensors = []
+    self.tensorFormat = []
+    self.input0Channels = []
+    self.inputHeight = []
+    self.inputWidth = []
+    self.inputShape = []
+    self.outputChannels = []
+    self.outputHeight = []
+    self.outputWidth = []
+    self.outputSize = []
+    self.outputShape = []
+
+
